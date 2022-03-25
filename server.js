2718e22e91f444c2c06e6a3c6171fa9bdf4e7801
santosharuraj <santoshraj90068@gmail.com>
2022-03-26 02:04:35 +0530

    add

--- server.js
+++ server.js
@@ -33,15 +33,15 @@ app.listen(Port, () => {
     console.log(`Listening at ${Port}`);
 })
 
-export let paytmMerchantkey = process.env.PAYTM_MERCHANT_KEY;
-export let paytmParams = {};
-paytmParams['MID'] = process.env.PAYTM_MID,
-    paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE,
-    paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID,
-    paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
-    paytmParams['ORDER_ID'] = uuid(),
-    paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
-    paytmParams['TXN_AMOUNT'] = '1',
-    paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback'
-paytmParams['EMAIL'] = 'codeforinterview01@gmail.com'
-paytmParams['MOBILE_NO'] = '1234567852'
+// export let paytmMerchantkey = process.env.PAYTM_MERCHANT_KEY;
+// export let paytmParams = {};
+// paytmParams['MID'] = process.env.PAYTM_MID,
+//     paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE,
+//     paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID,
+//     paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
+//     paytmParams['ORDER_ID'] = uuid(),
+//     paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
+//     paytmParams['TXN_AMOUNT'] = '1',
+//     paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback'
+// paytmParams['EMAIL'] = 'codeforinterview01@gmail.com'
+// paytmParams['MOBILE_NO'] = '1234567852'
